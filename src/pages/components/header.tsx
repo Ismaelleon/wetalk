7b08cf3d1d430e98eaf2f9f0c1b503f68c43131f
Ismaelleon <Ismaelleon@protonmail.com>
2023-04-07 13:07:48 -0300

    fix: change header padding

--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -1,6 +1,6 @@
 export default function Header () {
 	return (
-		<header className="flex justify-center fixed w-full h-fit inset-0 p-4 border-b border-neutral-400 bg-white">
+		<header className="flex justify-center fixed w-full h-fit inset-0 p-3 border-b border-neutral-400 bg-white">
 			<h1 className="text-3xl font-bold">WeTalk</h1>
 		</header>
 	);
